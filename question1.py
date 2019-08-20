22ad6c57c8c20e782d9612cbd29b965ffa39db0f
Emanuel Adler <adler@ic.ufal.br>
2019-08-20 00:15:35 -0300

    Question 1 answered
    Co-authored-by: Victor Accete <victor.accete@gmail.com>

---
+++ question1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt # plot graphs
+
+dfEven = pd.read_csv('eventos2015-2018.csv', low_memory=False, usecols=['idDeputado'])
+
+eventsCount = dfEven.groupby(['idDeputado']).size().to_frame('quantidadeEventos').reset_index().sort_values(by='quantidadeEventos', ascending=False)
+
+dfSpend = pd.read_csv('2015-2019.csv')
+congressPersons = dfSpend.groupby(['ideCadastro', 'txNomeParlamentar'])['vlrLiquido'].sum().to_frame(name = 'valorGasto').reset_index()
+congressPersons.rename(columns={'ideCadastro':'idDeputado'}, inplace=True)
+
+output = pd.merge(eventsCount, congressPersons, on='idDeputado')
+
+ax1 = output.plot.scatter(x='valorGasto', y='quantidadeEventos', c='DarkBlue')
+
+plt.show()
